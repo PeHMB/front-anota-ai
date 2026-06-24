b3b9ad90d130519fb8949d4379d76566a744eae2
rangelmarchiori <amandarangelmarchiori@gmail.com>
2026-06-23 22:30:41 -0400

    atualizações de produtos e pedidos

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,16 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
 using anotaai.Models;
 using anotaai.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace anotaai.Controllers;
 
 [Route("[controller]")]
 public class ProdutoController : Controller
 {
-    [HttpGet("")]
-    public IActionResult Index([FromServices] CardapioContext db)
+    private readonly CardapioContext _context;
+
+    public ProdutoController(CardapioContext context)
     {
-	var produtos = db.Produtos.ToList();
-	return View(produtos);
+        _context = context;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        var produtos = _context.Produtos.ToList();
+        return View(produtos);
+    }
+
+
+    [HttpGet("Edit/{id}")]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var produto = await _context.Produtos.FindAsync(id);
+        if (produto == null)
+            return NotFound();
+
+        return View(produto);
+    }
+
+    [HttpPost("Edit/{id}")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, ProdutoModel produto)
+    {
+        if (id != produto.Id)
+            return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(produto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(produto);
     }
 }
--- seed.sh
+++ seed.sh
@@ -4,6 +4,29 @@ http --verify=no POST http://localhost:5039/api/produtos \
   preco:=12.00
 
 http --verify=no POST http://localhost:5039/api/produtos \
+  nome="Tubaina da estrela" \
+  descricao="Bastante açucar" \
+  preco:=8.00
+
+http --verify=no POST http://localhost:5039/api/produtos \
+  nome="Lanche Feliz" \
+  descricao="ALFACE E TOMATE" \
+  preco:=4.00
+
+http --verify=no POST http://localhost:5039/api/produtos \
+  nome="bolonhesa" \
+  descricao="Tomate" \
+  preco:=10.00
+
+http --verify=no POST http://localhost:5039/api/produtos \
+  nome="SUBWAY" \
+  descricao="ALFACE E TOMATE" \
+  preco:=4.00
+# http --verify=no GET http://localhost:5039/api/produtos
+# http --verify=no GET http://localhost:5039/api/produtos/1
+# http --verify=no DELETE http://localhost:5039/api/produtos/1
+
+http --verify=no POST http://localhost:5039/api/produtos \
   nome="Coca-cola 350ml" \
   descricao="Bebida açúcarada" \
   preco:=4.50
@@ -12,12 +35,21 @@ http --verify=no POST http://localhost:5039/api/clientes \
   nome="Dona Dalva" \
   cpf="03142331120" \
   endereco="Rua das Flores 1232"
+http --verify=no POST http://localhost:5039/api/clientes \
+  nome="Juliano FLoss" \
+  cpf="00558898218" \
+  endereco="Rua das Flores 1232"
 
 http --verify=no POST http://localhost:5039/api/pedidos \
   Cliente:=1 \
   LinhasPedido:='[{"Produto":1,"Quantidade":2,"PrecoUnitario":12.00}]' \
   Obs="Sal marinho por favor"
 
-# http --verify=no DELETE http://localhost:5039/api/produtos/2
-# http --verify=no GET http://localhost:5039/api/produtos
+http --verify=no POST http://localhost:5039/api/pedidos \
+  Cliente:=2 \
+  LinhasPedido:='[{"Produto":2,"Quantidade":2,"PrecoUnitario":20.00}]' \
+  Obs="Sem açucar"
+
+
+
 # http --verify=no GET http://localhost:5039/api/pedidos/1
